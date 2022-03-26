9aa3b8e56651d6009f9ad66200d23759d1c3de33
Michael Dayan <michael.dayan@fcbg.ch>
2022-03-26 21:42:53 +0100

    Add ex5 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+  if (!(typeof int1 === "number") || !(typeof int2 === "number") || int1 < 0 || int2 < 0) {
+    return "ERROR"
+  }
+  const intMin = (int1 > int2) ? int2 : int1;
+  const intMax = (int1 > int2) ? int1 : int2;
+  let totalSum = 0
+  for (let i = intMin; i <= intMax; i++) {
+    totalSum += i;
+  }
+  return totalSum;
 };
 
 // Do not edit below this line
