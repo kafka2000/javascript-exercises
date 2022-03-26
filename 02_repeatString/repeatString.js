8033ec67a61c1b05bde9897e54fa29333c59777f
Michael Dayan <michael.dayan@fcbg.ch>
2022-03-26 17:06:04 +0100

    Add solutions for exercice 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+repeatString = function(stringToRepeat, numRepeat) {
+  repeatedString = ""
+  for (let i = 0; i < numRepeat; i++){
+    repeatedString += stringToRepeat;
+  }
+  if (numRepeat < 0) {
+    repeatedString = "ERROR";
+  }
+  return repeatedString;
+}
 
-};
+// console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
