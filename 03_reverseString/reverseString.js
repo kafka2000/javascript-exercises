07e5fa01c6ee1d9789ece901d02da0a1b9ee057f
Michael Dayan <michael.dayan@fcbg.ch>
2022-03-26 17:19:25 +0100

    Add ex3 solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+  let reversedString = "";
+  for (i = stringToReverse.length - 1; i >=0; i--) {
+    reversedString += stringToReverse[i];
+  }
+  return reversedString;
 };
+
+// console.log(reverseString("sdffff"));
 
 // Do not edit below this line
 module.exports = reverseString;
