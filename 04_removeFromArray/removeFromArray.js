744dd06fe072683273397234f95c10de3640438f
Michael Dayan <michael.dayan@fcbg.ch>
2022-03-26 21:20:07 +0100

    Add ex4 solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+"use strict";
 
+const removeFromArray = function(...args) {
+  let arrayToTrim = args[0];
+  let itemsToRemove = args.slice(1);
+  for (let item of itemsToRemove){
+    let removeIndex = arrayToTrim.findIndex(e => e===item);
+    if (removeIndex >= 0) {
+      arrayToTrim.splice(removeIndex, 1);
+    }
+  }
+  return arrayToTrim;
 };
 
 // Do not edit below this line
